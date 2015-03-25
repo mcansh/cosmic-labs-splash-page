a4b43f445cbf6c63f6d1531658f2e40eaf84931c
Logan McAnsh <logan@mcansh.com>
2015-03-24 22:27:43 -0400

    aye damon's got a new photo; and other changes

---
+++ js/tooltip-position.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(".tip-top").tooltip({
+	    placement : 'top',
+	});
+	$(".tip-right").tooltip({
+	    placement : 'right'
+	});
+	$(".tip-bottom").tooltip({
+	    placement : 'bottom'
+	});
+	$(".tip-left").tooltip({
+	    placement : 'left'
+	});
+	});
